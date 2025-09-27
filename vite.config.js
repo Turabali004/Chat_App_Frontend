10b59db215bad4c07985f120817fdb93e8d6f965
Turab Ali <turabalidev004@gmail.com>
2025-09-28 02:54:06 +0500

    Remove Node engine specification from package.json and update Vite configuration to disable sourcemaps in esbuild options.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    esbuildOptions: {
+      sourcemap: false,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
